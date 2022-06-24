78c143e7d468543ab08b3e045f8e2851ea3ded80
João Carli <46573038+jaocarli@users.noreply.github.com>
2022-06-23 21:04:11 -0300

    Aula 3 finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { Event } from './pages/Event';
+import { ApolloProvider } from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
+import { client } from './lib/apollo';
+import { Router } from './Router';
 
 export function App() {
   return (
-    <div>
-      <Event />
-    </div>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,13 +1,35 @@
+import { useParams } from 'react-router-dom';
 import { Header } from '../components/Header';
 import { Sidebar } from '../components/Sidebar';
 import { Video } from '../components/Video';
+import Lottie from 'lottie-react-web';
+import oops from '../animations/oops.json';
 
 export function Event() {
+  const { slug } = useParams<{ slug: string }>();
+  const bodyMovinOptions = {
+    loop: true,
+    prerender: true,
+  };
+
   return (
     <div className="flex flex-col">
       <Header />
       <main className="flex flex-1 min-h-screen">
-        <Video />
+        {slug ? (
+          <Video lessonSlug={slug} />
+        ) : (
+          <div className="flex-1 h-[600px] items-center">
+            <Lottie
+              options={{
+                animationData: oops,
+              }}
+            />
+            <h1 className="font-bold text-2xl block text-center text-green-300">
+              Clique em uma aula para acessar o conteúdo 🛸
+            </h1>
+          </div>
+        )}
         <Sidebar />
       </main>
     </div>
